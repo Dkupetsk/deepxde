7a1fa51e0ee5f4eb64a4e3d9d48bac189b58f59c
Dylan <dylan.kupetsky@nrel.gov>
2023-09-20 16:35:39 -0600

    modified:   deepxde/metrics.py 	modified:   deepxde/model.py

--- deepxde/model.py
+++ deepxde/model.py
@@ -829,10 +829,18 @@ class Model:
         )
 
         if isinstance(self.train_state.y_test, (list, tuple)):
+            from __main__ import sigmoid, lambdas, sigmoidprime, losshistory
             self.train_state.metrics_test = [
-                m(self.train_state.y_test[i], self.train_state.y_pred_test[i])
-                for m in self.metrics
-                for i in range(len(self.train_state.y_test))
+                num_train = 0
+                sa_lr = 1
+                while num_train < 25:
+                    gradl = sigmoidprime(lambdas)*losshistory.loss_train[-1]
+                    lambdas = lambdas + sa_lr*gradl
+                    self.metrics = [metrics_module.get(m) for m in metrics]
+                    m(self.train_state.y_test[i], self.train_state.y_pred_test[i])
+                    for m in self.metrics
+                    for i in range(len(self.train_state.y_test))
+                    num_train += 1
             ]
         else:
             from __main__ import sigmoid, lambdas, sigmoidprime
