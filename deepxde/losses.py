876ba4eeae83b51d9d3cc2e1150bd2a05c367230
Dylan <dylan.kupetsky@nrel.gov>
2023-09-22 09:47:11 -0600

    modified:   deepxde/losses.py

--- deepxde/losses.py
+++ deepxde/losses.py
@@ -26,6 +26,16 @@ def mean_squared_error(y_true, y_pred):
 def mean_l2_relative_error(y_true, y_pred):
     return bkd.reduce_mean(bkd.norm(y_true - y_pred, axis=1) / bkd.norm(y_true, axis=1))
 
+def l2_error_mask_sigmoid(y_true,y_pred):
+    from __main__ import lambdas, losshistory
+    def sigmoid(x):
+        return 100/(1 + bkd.exp(-5*(x - 1)))
+    def sigmoidprime(x):
+        return 500*bkd.exp(-5*(x-1))/(1 + bkd.exp(-5*(x-1))**2)
+    sa_lr = 1
+    gradl = sigmoidprime(lambdas)*losshistory.loss_train[-1]
+    lambdas = lambdas + sa_lr*gradl
+    return bkd.linalg.norm(sigmoid(lambdas)*(y_true - y_pred))
 
 def softmax_cross_entropy(y_true, y_pred):
     # TODO: pytorch
@@ -48,6 +58,7 @@ LOSS_DICT = {
     "MAPE": mean_absolute_percentage_error,
     "mape": mean_absolute_percentage_error,
     "mean l2 relative error": mean_l2_relative_error,
+    "l2 error mask sigmoid": l2_error_mask_sigmoid,
     "softmax cross entropy": softmax_cross_entropy,
     "zero": zero,
 }
