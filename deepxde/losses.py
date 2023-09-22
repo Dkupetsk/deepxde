d198c062fac0a13c6b7d92f8357d9f4faf47926c
Dylan <dylan.kupetsky@nrel.gov>
2023-09-22 11:37:03 -0600

    modified:   deepxde/losses.py

--- deepxde/losses.py
+++ deepxde/losses.py
@@ -1,5 +1,6 @@
 from . import backend as bkd
 from . import config
+import torch
 from .backend import tf
 
 
@@ -42,7 +43,7 @@ def l2_error_mask_sigmoid(y_true,y_pred):
 
 def uselambdas(y_true,y_pred):
     from __main__ import lambdas
-    return bkd.reduce_mean(bkd.matmul(bkd.transpose(lambdas),(y_true - y_pred)))
+    return bkd.reduce_mean(torch.dot(lambdas,(y_true - y_pred)))
 
 def softmax_cross_entropy(y_true, y_pred):
     # TODO: pytorch
