900cf9eb3cd60e9828b141d3ce5f5f89f842db3d
Dylan <dylan.kupetsky@nrel.gov>
2023-09-25 09:43:35 -0600

    modified:   deepxde/model.py 	modified:   deepxde/utils/internal.py

--- deepxde/model.py
+++ deepxde/model.py
@@ -829,7 +829,7 @@ class Model:
         )
 
         if isinstance(self.train_state.y_test, (list, tuple)):
-            from __main__ import lambdas, losshistory
+            #from __main__ import lambdas, losshistory
             self.train_state.metrics_test = [
                 m(self.train_state.y_test[i], self.train_state.y_pred_test[i])
                 for m in self.metrics
