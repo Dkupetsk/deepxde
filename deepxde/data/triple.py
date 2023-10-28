c95005dfe57bdc62b04dde2b24788fe753d27200
Dkupetsk <143652984+Dkupetsk@users.noreply.github.com>
2023-10-28 09:22:16 -0600

    Update triple.py

--- deepxde/data/triple.py
+++ deepxde/data/triple.py
@@ -59,11 +59,11 @@ class TripleCartesianProd(Data):
     """
 
     def __init__(self, X_train, y_train, X_test, y_test):
-        if len(X_train[0]) * len(X_train[1]) != y_train.size:
+        if len(X_train[0][0]) * len(X_train[1]) != y_train.size:
             raise ValueError(
                 "The training dataset does not have the format of Cartesian product."
             )
-        if len(X_test[0]) * len(X_test[1]) != y_test.size:
+        if len(X_test[0][0]) * len(X_test[1]) != y_test.size:
             raise ValueError(
                 "The testing dataset does not have the format of Cartesian product."
             )
