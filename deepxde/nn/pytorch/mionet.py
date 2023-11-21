0d99b1f3a4c0b3a6890de9926759f32698f3b44d
Dkupetsk <143652984+Dkupetsk@users.noreply.github.com>
2023-11-21 11:46:12 -0700

    Update mionet.py

--- deepxde/nn/pytorch/mionet.py
+++ deepxde/nn/pytorch/mionet.py
@@ -121,7 +121,7 @@ class MIONetCartesianProd(NN):
             )
         # output merger net
         if self.output_merger is None:
-            y = torch.einsum("ip,jp->ij", y_func, y_loc)
+            y = torch.einsum("ip,ijp->ij", y_func, y_loc)
         else:
             y_func = y_func[:, None, :]
             y_loc = y_loc[None, :]
